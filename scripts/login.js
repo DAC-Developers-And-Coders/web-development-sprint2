69c708d0c2a3d4b79ab6dcc78a7d9343eec06511
Gabriel Novaga Pereira <gnovaga@hotmail.com>
2026-05-12 22:05:20 -0300

    criando o JavaScript com o login

---
+++ scripts/login.js
@@ -0,0 +1,49 @@
+function register(){
+    const login = document.getElementById('user').value
+    const password = document.getElementById('password').value 
+
+    if(login === '' || password === ''){
+        alert('falta preencher a senha ou o usuário')
+        return
+    }
+
+    const users = JSON.parse(localStorage.getItem('users')) || []
+    const existentUsers = users.find(user => user.login === login)
+
+    if(existentUsers){
+        alert('Usuário já existe')
+        return
+    }
+
+    users.push({
+        login:login,
+        password:password
+    })
+
+    localStorage.setItem('users', JSON.stringify(users))
+    alert('Usuário cadastrado')
+}
+
+function login(){
+    const login = document.getElementById('user').value
+    const password = document.getElementById('password').value 
+    
+    if(login === "admin" && password === "admin"){
+        alert("Login realizado com sucesso");
+        window.location.href = "index.html";
+        return;
+    }
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const validUser = users.find(user =>
+        user.login === login && user.password === password
+    );
+
+    if(validUser){
+        alert("Login realizado com sucesso");
+        window.location.href = "index.html";
+    } else {
+        alert("Usuário ou senha incorretos");
+    }
+}
